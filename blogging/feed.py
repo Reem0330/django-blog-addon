8b05bc6c2c85c06979ec518ffffe3d70617e0eef
Reem0330 <reem3@uw.edu>
2020-12-08 23:40:45 -0800

    adding RSS

---
+++ blogging/feed.py
@@ -0,0 +1,17 @@
+from django.contrib.syndication.views import Feed
+from blogging.models import Post
+
+
+class LatestEntriesFeed(Feed):
+    title = "Latest Posts"
+    link = "/latest/feed"
+    description = "Updates on changes and additions to blog posts"
+
+    def items(self):
+        return Post.objects.order_by('created_date')[:5]
+
+    def item_title(self, item):
+        return item.title
+
+    def item_description(self, item):
+        return item.text
